1a43641150b3f9ca34c7027e912d5fb0914d9031
Sena Sönmez <77552150+senaSonmez156@users.noreply.github.com>
2022-03-03 18:05:32 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,18 +1,32 @@
-package vucudKitleIndexHesapla;
+package manav;
 import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		double boy,kilo;
+		double armut=2.14, elma=3.67, domates=1.11, muz=0.95, patlýcan=5.00;
+		double armutKg, elmaKg,muzKg,patlýcanKg, domatesKg;
 		Scanner input=new Scanner(System.in);
-		System.out.print("Merhaba, Hoþgeldiniz :) \nBoyunuzu (metre cinsinden)giriniz: ");
-		boy=input.nextDouble();
-		System.out.print("Lütfen kilonuzu giriniz: ");
-		kilo=input.nextDouble();
+		System.out.print("Hoþgeldiniz :)");
+		System.out.print("armut kaç kilo? ");
+		armutKg= input.nextDouble();
+		System.out.print("Elma kaç kilo? ");
+		elmaKg=input.nextDouble();
+		System.out.print("Domates kaç kilo? ");
+		domatesKg=input.nextDouble();
+		System.out.print("Muz kaç kilo? ");
+		muzKg=input.nextDouble();
+		System.out.print("Patlýcan kaç kilo? ");
+		patlýcanKg=input.nextDouble();
 		
-		double index= boy/ (kilo*kilo);
-		System.out.println("Vücut kitle index'iniz: " +index);
+		
+		double toplam= (armut*armutKg)+(elma*elmaKg)+(patlýcan*patlýcanKg)+(domates*domatesKg)+(muz*muzKg);
+		System.out.println("Toplam tutarýnýz: "+ toplam);
+		
+		
+		
+		
+		
+		
 
 	}
 
