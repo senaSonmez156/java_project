ae61b4d67a52379e3f313c501278561b6f798c3f
Sena Sönmez <77552150+senaSonmez156@users.noreply.github.com>
2022-03-05 20:19:24 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,51 +1,46 @@
-package cinZodyagý;
+package dongulerOrnek1;
 import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		int birthYear;
 		Scanner input=new Scanner(System.in);
-		System.out.print("Doðum yýlýnýzý giriniz: ");
-		birthYear=input.nextInt();
-		if(birthYear%12==0) {
-			System.out.print("Çin zodyaðýna göre burcunuz: Maymun ");
-		}
-		if(birthYear%12==1) {
-			System.out.print("Çin zodyaðýna göre burcunuz: Horoz ");
-		}
-		if(birthYear%12==2) {
-			System.out.print("Çin zodyaðýna göre burcunuz: Köpek");
-		}
-		if(birthYear%12==3) {
-			System.out.print("Çin zodyaðýna göre burcunuz: Domuz");
-		}
-		if(birthYear%12==4) {
-			System.out.print("Çin zodyaðýna göre burcunuz: Fare");
-		}
-		if(birthYear%12==5) {
-			System.out.print("Çin zodyaðýna göre burcunuz: Öküz");
-		}
-		if(birthYear%12==6) {
-			System.out.print("Çin zodyaðýna göre burcunuz: Kaplan");
-		}
-		if(birthYear%12==7) {
-			System.out.print("Çin zodyaðýna göre burcunuz: Tavþan");
-		}
-		if(birthYear%12==8) {
-			System.out.print("Çin zodyaðýna göre burcunuz: Ejderha");
-		}
-		if(birthYear%12==9) {
-			System.out.print("Çin zodyaðýna göre burcunuz: Yýlan");
-		}
-		if(birthYear%12==10) {
-			System.out.print("Çin zodyaðýna göre burcunuz: At");
-		}
-		if(birthYear%12==11) {
-			System.out.print("Çin zodyaðýna göre burcunuz: Koyun");
+		System.out.print("Sayýyý giriniz: ");
+		int sayi=input.nextInt();
+		
+		int toplam=0;
+		int n=0;
+				
+		for(int i=0; i<sayi; i++) {
+			
+			if((i%3==0) && (i%4==0)) {
+				System.out.println("Bölünen sayýlar: " + i);
+				toplam=toplam+n;
+				n++;
+				
+			}
 		}
 		
-		
+		System.out.print("Toplam: " + toplam + "\nAdet: " +n);
+		int ortalama=toplam/n;
 
+		System.out.print("\nOrtalama: " + ortalama);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
 	}
 
 }
