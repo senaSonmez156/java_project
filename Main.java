631ca7722819d653d14d7a8a068a71a6e408fc0d
Sena Sönmez <77552150+senaSonmez156@users.noreply.github.com>
2022-03-09 18:59:31 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,49 +1,21 @@
-package faktoriyel;
+package usluSayilar;
 import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		int r,n;
 		Scanner scan=new Scanner(System.in);
-		System.out.print("c(n,r)kombinasyonu için: ");
-		System.out.print("n sayýsýný giriniz: " );
-		n=scan.nextInt();
-		System.out.print("r sayýsýný giriniz: " );
-		r=scan.nextInt();
+		int k,u;
+		System.out.print("Sayýyý girniz: ");
+		 k=scan.nextInt();
+		System.out.print("Üsü yazýnýz: ");
+		 u=scan.nextInt();
+		 int total=1;
+		 
+		 for(int i=1; i <= u ; i++) { 
+			 total*=k;
+		 }
 		
-		int totaln=1, totalr=1, totalneksir=1;
-		
-		for(int i=1; i<=n; i++) {
-			totaln=totaln*i;
-			
-		}
-		System.out.print("n!:" + totaln);
-		
-		
-		for(int j=1; j<=r; j++ ) {
-			totalr=totalr*j;
-		}
-		System.out.print("r!:" + totalr);
-		
-		  System.out.println("r!= "+totalr);
-	        for(int k=1;k<=(n-r);k++){
-	            totalneksir=totalneksir*k;
-	        }
-	        System.out.println("(n-r)!= "+totalneksir);
-	        double sonuc=totaln/(totalr*(totalneksir));
-	        System.out.println(sonuc);
-
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		 System.out.print("Cevap: " + total);
 
 	}
 
