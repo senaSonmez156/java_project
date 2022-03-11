02a4d5d5aa9c137a7a1f044e95731358f5f6e3d3
Sena Sönmez <77552150+senaSonmez156@users.noreply.github.com>
2022-03-11 19:05:03 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,24 +1,22 @@
-package metotlarUsluSayilar;
+package basamakSayý;
 import java.util.Scanner;
 public class Main {
-	static int power(int x,int y) {
-		int result=1;
-		for(int i=1; i<=y; i++)
-		{
-			result *=x;
-		}
-		return result;
-	}
 
 	public static void main(String[] args) {
 	Scanner scan=new Scanner(System.in);
 	System.out.print("Sayýyý giriniz: ");
-	int a=scan.nextInt();
-	System.out.print("Üssü giriniz: ");
-	int b=scan.nextInt();
+	int number=scan.nextInt();
+	int basValue;
+	int total=0;
 	
-	System.out.println(power(a,b));
+	while(number!=0)
+	{
+		basValue=number%10;
+		total+=basValue;
+		number/=10;
+	}
 
+	System.out.print(total);
 	}
 
 }
