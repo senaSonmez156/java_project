17e34c0c767f03b12141c41a86dcb6fdac0831c3
Sena Sönmez <77552150+senaSonmez156@users.noreply.github.com>
2022-03-19 18:32:37 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,191 +1,20 @@
-package horoscopeFinderProgram;
-import java.util.Scanner;
+package dizilerAvarange;
+
 public class Main {
 
 	public static void main(String[] args) {
-		int month,day;
-		Scanner scan=new Scanner(System.in);
-		System.out.println("Doğduğunuız ayı giriniz: ");
-		month=scan.nextInt();
-		System.out.println("Doğduğunuz günü yazınız: ");
-		day=scan.nextInt();
-		
-		String horoscope="";
-		boolean isError=false;
-		
-		if(month==1) {
-		if(day>=1 && day<=31) {
-			if(day<21) {
-				horoscope="Oğlak";
-			}
-			else {
-				horoscope="Kova";
-			}
-			
-		}else {
-			isError=true;
-		}}
-
-		else if(month==2) {
-		if(day>=1 && day<=28) {
-			if(day<20) {
-				horoscope="Kova";
-			}
-			else {
-				horoscope="Balık";
-			}
-			
-		}else {
-			isError=true;
-		}}
-		
-
-		else if(month==3) {
-		if(day>=1 && day<=31) {
-			if(day<21) {
-				horoscope="Balık";
-			}
-			else {
-				horoscope="Koç";
-			}
-			
-		}else {
-			isError=true;
-		}}
-		
-
-		else if(month==4) {
-		if(day>=1 && day<=30) {
-			if(day<21) {
-				horoscope="Koç";
-			}
-			else {
-				horoscope="Boğa";
-			}
-			
-		}else {
-			isError=true;
-		}}
-		
-
-		else if(month==5) {
-		if(day>=1 && day<=31) {
-			if(day<22) {
-				horoscope="Boğa";
-			}
-			else {
-				horoscope="İkizler";
-			}
-			
-		}else {
-			isError=true;
-		}}
-		
-
-		else if(month==6) {
-		if(day>=1 && day<=30) {
-			if(day<23) {
-				horoscope="İkizler";
-			}
-			else {
-				horoscope="Yengeç";
-			}
-			
-		}else {
-			isError=true;
-		}}
-		
-
-		else if(month==7) {
-		if(day>=1 && day<=31) {
-			if(day<23) {
-				horoscope="Yengeç";
-			}
-			else {
-				horoscope="Aslan";
-			}
-			
-		}else {
-			isError=true;
-		}}
-		
-
-		else if(month==8) {
-		if(day>=1 && day<=31) {
-			if(day<23) {
-				horoscope="Aslan";
-			}
-			else {
-				horoscope="Başak";
-			}
-			
-		}else {
-			isError=true;
-		}}
-		
-
-		else if(month==9) {
-		if(day>=1 && day<=30) {
-			if(day<23) {
-				horoscope="Başak";
-			}
-			else {
-				horoscope="Terazi";
-			}
-			
-		}else {
-			isError=true;
-		}}
-		
-
-		else if(month==10) {
-		if(day>=1 && day<=31) {
-			if(day<23) {
-				horoscope="Terazi";
-			}
-			else {
-				horoscope="Akrep";
-			}
-			
-		}else {
-			isError=true;
-		}}
-
-		else if(month==11) {
-		if(day>=1 && day<=30) {
-			if(day<22) {
-				horoscope="Akrep";
-			}
-			else {
-				horoscope="Yay";
-			}
-			
-		}else {
-			isError=true;
-		}}
-		
-
-		else if(month==12) {
-		if(day>=1 && day<=31) {
-			if(day<22) {
-				horoscope="Yay";
-			}
-			else {
-				horoscope="Oğlak";
-			}
-			
-		}else {
-			isError=true;
-		}}
-
-		else {
-			isError=true;
-		}
-		
-
-		if(isError) {System.out.println("Hatalı bilgi girdiniz. Kontrol ediniz.");}
-		else {System.out.println("Burcunuz: " + horoscope );}
-	}
 	
+		int[] numbers= {1,2,3,4,5,6};
+		double harmonicSeries=0,average=0;
+		
+		
+		for(int i=0;i<numbers.length;i++) {
+			harmonicSeries+=1.0/numbers[i];
+		}
+		average=numbers.length/harmonicSeries;
+		
+		System.out.println("Harmonik Ortalama: "+average);
+
+	}
 
 }
